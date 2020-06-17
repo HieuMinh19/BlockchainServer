f7e4488abc43c4ec946a9420801f980820732b73
hieule <lehieuminh231@gmail.com>
2020-06-18 00:14:03 +0700

    update models

--- src/chain_server/urls.py
+++ src/chain_server/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path
-from core.views import  register, get_last_block, get_signature, test_view
-
+from core.views import register, get_last_block, test_view
+from core.views import create_transaction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^register/$', register, name='register_user'),
-    url(r'^get-signature', get_signature, name='get_signature'),
+    url(r'^transaction/create', create_transaction, name='get_signature'),
     url(r'^test_view', test_view, name='test'),
     url(r'^chain/get-last-block/$', get_last_block, name='chain.last_block'),
 ]
--- src/core/models.py
+++ src/core/models.py
@@ -11,133 +11,12 @@ import hashlib
 import ecdsa
 import eth_keys, os
 import MySQLdb
-class KeyGenerator:
-    def __init__(self):
-        self.POOL_SIZE = 256
-        self.KEY_BYTES = 32
-        # n = 1.158 x 10^17
-        self.CURVE_ORDER = int('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141', 16)
-        self.pool = [0] * self.POOL_SIZE
-        self.pool_pointer = 0
-        self.prng_state = None
-        self.__init_pool()
-        self.string = ''
-        
-    def seed_input(self, str_input):
-        time_int = int(time.time())
-        self.__seed_int(time_int)
-        """
-        vòng for dưới đây mang ý nghĩa:
-        đưa 1 chuỗi các bit ngẫu nhiên vào một hàm băm SHA256
-        -> tạo ra một số 256 bit.
-        """
-        for char in str_input:
-            char_code = ord(char)  
-            self.__seed_byte(char_code)
-            
-    def generate_key(self):
-        big_int = self.__generate_big_int()
-        big_int = big_int % (self.CURVE_ORDER - 1) # key < curve order
-        big_int = big_int + 1 # key > 0
-        key = hex(big_int)[2:]
-        # Add leading zeros if the hex key is smaller than 64 chars
-        key = key.zfill(self.KEY_BYTES * 2)
-        return key
-
-    def __init_pool(self):
-        for i in range(self.POOL_SIZE):
-            random_byte = secrets.randbits(8)
-            self.__seed_byte(random_byte)
-        time_int = int(time.time())
-        self.__seed_int(time_int)
-
-    def __seed_int(self, n):
-        self.__seed_byte(n)
-        self.__seed_byte(n >> 8)            #comback 8 bit    
-        self.__seed_byte(n >> 16)
-        self.__seed_byte(n >> 24)
-
-    def __seed_byte(self, n):
-        self.pool[self.pool_pointer] ^= n & 255  # & => Sets each bit to 1 if both bits are 1
-        self.pool_pointer += 1
-        if self.pool_pointer >= self.POOL_SIZE:
-            self.pool_pointer = 0
-    
-    def __generate_big_int(self):
-        if self.prng_state is None:
-            seed = int.from_bytes(self.pool, byteorder='big', signed=False)
-            random.seed(seed)
-            self.prng_state = random.getstate()
-        random.setstate(self.prng_state)
-        big_int = random.getrandbits(self.KEY_BYTES * 8)
-        self.prng_state = random.getstate()
-        return big_int
+import binascii
 
 
 class BitcoinWallet:
     def __init__(self):
         super().__init__()
-
-    def generate_address(self, private_key):
-        public_key = BitcoinWallet.private_to_public(self, private_key)
-        address = BitcoinWallet.public_to_address(self, public_key)
-        return address
-    
-    def private_to_public(self, private_key):
-        private_key_bytes = codecs.decode(private_key, 'hex')
-        # Get ECDSA public key
-        key = ecdsa.SigningKey.from_string(private_key_bytes, curve=ecdsa.SECP256k1).verifying_key
-        key_bytes = key.to_string()
-        key_hex = codecs.encode(key_bytes, 'hex')
-        # Add bitcoin byte
-        bitcoin_byte = b'04'
-        public_key = bitcoin_byte + key_hex
-        return public_key
-
-    def public_to_address(self, public_key):
-        public_key_bytes = codecs.decode(public_key, 'hex')
-        # Run SHA256 for the public key
-        sha256_bpk = hashlib.sha256(public_key_bytes)
-        sha256_bpk_digest = sha256_bpk.digest()     #tranfer to byte type
-        # Run ripemd160 for the SHA256
-        ripemd160_bpk = hashlib.new('ripemd160')
-        ripemd160_bpk.update(sha256_bpk_digest)
-        ripemd160_bpk_digest = ripemd160_bpk.digest()
-        ripemd160_bpk_hex = codecs.encode(ripemd160_bpk_digest, 'hex')
-        # Add network byte
-        network_byte = b'00'
-        network_bitcoin_public_key = network_byte + ripemd160_bpk_hex
-        network_bitcoin_public_key_bytes = codecs.decode(network_bitcoin_public_key, 'hex')
-        # Double SHA256 to get checksum
-        sha256_nbpk = hashlib.sha256(network_bitcoin_public_key_bytes)
-        sha256_nbpk_digest = sha256_nbpk.digest()
-        sha256_2_nbpk = hashlib.sha256(sha256_nbpk_digest)
-        sha256_2_nbpk_digest = sha256_2_nbpk.digest()
-        sha256_2_hex = codecs.encode(sha256_2_nbpk_digest, 'hex')
-        checksum = sha256_2_hex[:8]
-        # Concatenate public key and checksum to get the address
-        address_hex = (network_bitcoin_public_key + checksum).decode('utf-8')
-        wallet = BitcoinWallet.base58(self, address_hex)
-        return wallet
-
-    def base58(self, address_hex):
-        alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-        b58_string = ''
-        # Get the number of leading zeros and convert hex to decimal
-        leading_zeros = len(address_hex) - len(address_hex.lstrip('0'))
-        # Convert hex to decimal
-        address_int = int(address_hex, 16)
-        # Append digits to the start of string
-        while address_int > 0:
-            digit = address_int % 58
-            digit_char = alphabet[digit]
-            b58_string = digit_char + b58_string
-            address_int //= 58
-        # Add '1' for each 2 leading zeros
-        ones = leading_zeros // 2
-        for one in range(ones):
-            b58_string = '1' + b58_string
-        return b58_string
 
     #-------------using eth_keys, os library-------------
     def generate_private_key(self):
@@ -148,32 +27,33 @@ class BitcoinWallet:
     def generate_public_key(self, private_key):
         return (private_key.public_key)
 
-    def generate_signature(self, private_key, msg):
-        private = eth_keys.keys.PrivateKey(private_key)
-        print(private)        
-        signature = private.sign_msg(msg)
-        return signature
+    '''
+    recover private key from string
+    @params: string privateKey
+    @return PrivateKey object
+    @author HieuLe
+    '''
+    def recover_private_key(self, privateKey):
+        byteHexPriv = privateKey.encode()
+        bytePriv = binascii.unhexlify(byteHexPriv)
+        priv = eth_keys.keys.PrivateKey(bytePriv)
+        return priv
+
     #-------------end using eth_keys, os library-------------
 
 class Blockchain:
-    difficulty = 2
     def __init__(self):
-        """
-        Constructor của class `Blockchain`.
-        """
-        self.chain = [] 
-        self.createGenesisBlock()
+        super().__init__()
 
-    def createGenesisBlock(self):
-        GenesisBlock = Block(0, "0", 1465154705, "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7", 0)
-        self.chain.append(GenesisBlock)
+    # def createGenesisBlock(self):
+    #     GenesisBlock = Block(0, "0", 1465154705, "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7", 0)
+    #     self.chain.append(GenesisBlock)
 
     # def getLatestBlock(self):
     #     return self.chain[-1]
 
     def getLatestBlock(self):
         db_ = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="",db="blockchain")
-        db_.autocommit(True)
         db_cursor = db_.cursor()
         sql = "SELECT * FROM blocks ORDER by id DESC limit 1"
         db_cursor.execute(sql)
@@ -295,4 +175,22 @@ class TransactionOutput:
         self.tx_index = tx_index
         self.script_sign = script_sig
 
+    '''
+    get all un spend trans_output by signature
+    @param eth_object signature
+    @param String msg
+    '''
+    def get_by_signature(self, signature, msg):
+        publicKey = signature.recover_public_key_from_msg(msg)
+        db_ = MySQLdb.connect(
+            host="localhost", 
+            port=3306, 
+            user="root", 
+            passwd="", 
+            db="blockchain")
+        db_cursor = db_.cursor()
+        sql = "SELECT * FROM trans_output WHERE public_key ="
+        sql += publicKey[2:].encode()
+        db_cursor.execute(sql)
+        result = db_cursor.fetchall()
 
